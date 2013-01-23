6bc19e99377f0335110b640fc7ad0d6a684c7f0a
viper6delta <jwhitney114@gmail.com>
2013-01-23 14:20:09 -0500

    decorations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Hittfit::Application.routes.draw do
+
+ 
+root :to => 'home#index'
+
+
+
+  get "home/index"
+
+
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +59,7 @@ Hittfit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  # 
 
   # See how all your routes lay out with "rake routes"
 
