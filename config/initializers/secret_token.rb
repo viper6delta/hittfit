ccc7ad586b56bb2914bcf2af12eda80706532d2d
viper6delta <jwhitney114@gmail.com>
2013-01-23 12:30:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hittfit::Application.config.secret_token = '0aae4314d3dc52a22ae1f33898b150619b903b8ace41468147c21e3e3edf96935d0d5c005b5e095cf9a75f3e972e6d4ecb66ffd0dbff070ee2488d4559958e87'
